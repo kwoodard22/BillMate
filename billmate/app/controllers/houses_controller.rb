838ba551234477f1faf9bcf1b87396fe0ddec3bb
kwoodard22 <kwoodard22@gmail.com>
2014-10-28 15:40:52 -0400

    Worked on edit page of house profile, but having issues
    Need to go back and delete railscast’s javascript and do a ‘new roommates’ page.

--- billmate/app/controllers/houses_controller.rb
+++ billmate/app/controllers/houses_controller.rb
@@ -1,9 +1,10 @@
 class HousesController < ApplicationController
-  # before_action :set_house, only: [:show, :edit, :update, :destroy, :roommates]
-  # Took the above from another tutorial and not sure if needed.
-  
   def index
     @houses = House.all
+  end
+
+  def show
+    @house = House.find(params[:id])
   end
 
   def new
@@ -16,15 +17,11 @@ class HousesController < ApplicationController
 
     respond_to do |format|
       if @house.save
-        format.html { redirect_to @house, notice: 'House profile was successfully created.' }
+        format.html { redirect_to @house, notice: 'Successfully created house profile.' }
       else
         format.html { render :new, notice: 'Creating house profile failed.' }
       end
     end
-  end
-
-  def show
-    @house = House.find(params[:id])
   end
 
   def edit
@@ -32,15 +29,16 @@ class HousesController < ApplicationController
   end
 
   def update
-    if @house.update(house_params)
-      redirect_to @house, notice: "House profile was successfully updated."
+    @house = House.find(params[:id])
+    if @house.update_attributes(house_params)
+      redirect_to @house, notice: "Successfully updated house profile."
     else
       render :edit
     end
   end
 
   def destroy
-
+    @house = House.find(params[:id])
     if @house.destroy
       redirect_to root_path, notice: "House profile successfully destroyed."  # this essentially redirects to our index page
     else
